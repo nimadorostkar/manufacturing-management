ac773896a73b0eba8e937094837849ba00798add
nimadorostkar <nimadorostkar97@gmail.com>
2021-01-21 14:10:10 +0330

    admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.site_header= "  پنل مدیریت  "
+admin.site.site_title= "Tavankar"
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ('id','name','title')
+
+
+admin.site.register(models.Product,ProductAdmin)
