644b081683e27b1772c44a9c61d8087fc5ef0415
nimadorostkar <nimadorostkar97@gmail.com>
2021-01-24 16:43:43 +0330

    True

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ PROJECT_DIR = Path(__file__).parent
 SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False)
+DEBUG = config('DEBUG', default=True)
 
 # load production server from .env
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
