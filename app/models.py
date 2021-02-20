83872d473f9495e67936d819764e71a266530c95
nimadorostkar <nimadorostkar97@gmail.com>
2021-02-20 14:50:24 +0330

    rm relation

--- app/models.py
+++ app/models.py
@@ -5,26 +5,10 @@ from django.urls import reverse
 
 
 
-
-#------------------------------------------------------------------------------
-class Material(models.Model):
-    name=models.CharField(max_length=400,verbose_name = "نام")
-    description=models.TextField(max_length=500,null=True, blank=True,verbose_name = "مشخصات")
-    stationInput=models.ManyToManyField(Station,null=True,blank=True,verbose_name = " ایستگاه ")
-
-    class Meta:
-        verbose_name = "قطعه"
-        verbose_name_plural = "قطعات"
-
-    def __str__(self):
-        return self.name
-
-
 #------------------------------------------------------------------------------
 class Station(models.Model):
     name=models.CharField(max_length=400,verbose_name = "نام")
     description=models.TextField(max_length=500,null=True, blank=True,verbose_name = "مشخصات")
-    input=models.ManyToManyField(Material,null=True,blank=True,verbose_name = " قطعه ")
 
     class Meta:
         verbose_name = "ایستگاه"
@@ -35,11 +19,23 @@ class Station(models.Model):
 
 
 #------------------------------------------------------------------------------
+class Material(models.Model):
+    name=models.CharField(max_length=400,verbose_name = "نام")
+    description=models.TextField(max_length=500,null=True, blank=True,verbose_name = "مشخصات")
+
+    class Meta:
+        verbose_name = "قطعه"
+        verbose_name_plural = "قطعات"
+
+    def __str__(self):
+        return self.name
+
+
+
+#------------------------------------------------------------------------------
 class Product(models.Model):
     name=models.CharField(max_length=200,verbose_name = "نام")
     description=models.TextField(max_length=800,null=True,blank=True,verbose_name = "توضیحات")
-    materialInput=models.ManyToManyField(Material,null=True,blank=True,verbose_name = " قطعه ")
-    stationInput=models.ManyToManyField(Station,null=True,blank=True,verbose_name = " ایستگاه ")
 
     class Meta:
         verbose_name = " محصول "
