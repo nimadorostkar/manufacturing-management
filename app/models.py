386a7d2a7ad884a776abdcffe3d50b5fa1ac5acd
nimadorostkar <nimadorostkar97@gmail.com>
2021-02-20 12:04:49 +0330

    models

--- app/models.py
+++ app/models.py
@@ -4,18 +4,45 @@ from django.utils.html import format_html
 from django.urls import reverse
 
 
+
+
+#------------------------------------------------------------------------------
+class Station(models.Model):
+    name=models.CharField(max_length=400,verbose_name = "نام")
+    description=models.TextField(max_length=500,null=True, blank=True,verbose_name = "مشخصات")
+    input=models.ManyToManyField(Material,null=True,blank=True,verbose_name = " قطعه ")
+
+    class Meta:
+        verbose_name = "ایستگاه"
+        verbose_name_plural = "ایستگاه ها"
+
+    def __str__(self):
+        return self.name
+
+
+#------------------------------------------------------------------------------
+class Material(models.Model):
+    name=models.CharField(max_length=400,verbose_name = "نام")
+    description=models.TextField(max_length=500,null=True, blank=True,verbose_name = "مشخصات")
+    input=models.ManyToManyField(Station,null=True,blank=True,verbose_name = " ایستگاه ")
+
+    class Meta:
+        verbose_name = "قطعه"
+        verbose_name_plural = "قطعات"
+
+    def __str__(self):
+        return self.name
+
+
+#------------------------------------------------------------------------------
 class Product(models.Model):
     name=models.CharField(max_length=200,verbose_name = "نام")
-    description=models.TextField(max_length=800,null=True, blank=True,verbose_name = "توضیحات")
-    input=models.ManyToManyField(Material,verbose_name = " قطعه ")
-
+    description=models.TextField(max_length=800,null=True,blank=True,verbose_name = "توضیحات")
+    input=models.ManyToManyField(Material,null=True,blank=True,verbose_name = " قطعه ")
 
     class Meta:
         verbose_name = " محصول "
         verbose_name_plural = " محصولات "
 
-
     def __str__(self):
         return self.name
-    def get_absolute_url(self):
-        return reverse('App:product_detail',args=[self.id])
