3ac72f966cb95cc5aa97c13f9a450b3c9f6a1fa4
nimadorostkar <nimadorostkar97@gmail.com>
2021-02-20 11:47:19 +0330

    define Product

--- app/models.py
+++ app/models.py
@@ -5,18 +5,17 @@ from django.urls import reverse
 
 
 class Product(models.Model):
-    id=models.AutoField(primary_key=True,verbose_name = "شناسه")
-    pid=models.CharField(max_length=100,verbose_name = "زیر شاخه")
-    name=models.CharField(max_length=100,verbose_name = "نام")
-    title=models.CharField(max_length=100,verbose_name = "تیتر")
-    image=models.ImageField(upload_to='image',verbose_name = "تصویر")
-    content=models.TextField(max_length=500,verbose_name = "مشخصات")
-    tag=models.CharField(max_length=200,verbose_name = "برچسب")
+    name=models.CharField(max_length=200,verbose_name = "نام")
+    description=models.TextField(max_length=800,null=True, blank=True,verbose_name = "توضیحات")
+    input=models.ManyToManyField(Material,verbose_name = " قطعه ")
 
 
     class Meta:
-        verbose_name = "محصول"
-        verbose_name_plural = "محصولات"
+        verbose_name = " محصول "
+        verbose_name_plural = " محصولات "
+
 
     def __str__(self):
         return self.name
+    def get_absolute_url(self):
+        return reverse('App:product_detail',args=[self.id])
