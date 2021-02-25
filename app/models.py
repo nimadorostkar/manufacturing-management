adc6c70bb311d15b7d6471cc294b4dbe99a1652f
nimadorostkar <nimadorostkar97@gmail.com>
2021-02-25 11:59:05 +0330

    Unsuccessful changes

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 
 
 
+
 #------------------------------------------------------------------------------
 class Material(models.Model):
     #id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
@@ -24,7 +25,7 @@ class Material(models.Model):
 
 #------------------------------------------------------------------------------
 class Station(models.Model):
-    #id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    id = models.AutoField(auto_created=True, primary_key=True, verbose_name='ID')
     name=models.CharField(max_length=400,verbose_name = "نام")
     description=models.TextField(max_length=500,null=True, blank=True,verbose_name = "مشخصات")
     #m_sub=models.ForeignKey(Material,on_delete=models.CASCADE,blank=True,verbose_name = " قطعه ورودی")
--- core/settings.py
+++ core/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app'  # Enable the inner app
+
 ]
 
 MIDDLEWARE = [
