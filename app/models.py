2940229ad95bba53bc7cd3b3cc25ac5189d46f31
nimadorostkar <nimadorostkar97@gmail.com>
2021-02-27 14:47:28 +0330

    comment all models

--- app/models.py
+++ app/models.py
@@ -5,67 +5,68 @@ from django.urls import reverse
 
 
 
+
 #------------------------------------------------------------------------------
-class Material(models.Model):
+#class Material(models.Model):
     #id = models.AutoField(auto_created=True, primary_key=True, verbose_name='ID')
-    name=models.CharField(max_length=400,verbose_name = "نام")
-    description=models.TextField(max_length=500,null=True, blank=True,verbose_name = "مشخصات")
+    #name=models.CharField(max_length=400,verbose_name = "نام")
+    #description=models.TextField(max_length=500,null=True, blank=True,verbose_name = "مشخصات")
     #material_input=models.ForeignKey('Material',on_delete=models.CASCADE,null=True,blank=True,verbose_name = " قطعه ورودی")
     #station_input=models.ForeignKey('Station',on_delete=models.CASCADE,null=True,blank=True,verbose_name = " ایستگاه ورودی ")
 
-    class Meta:
-        verbose_name = "قطعه"
-        verbose_name_plural = "قطعات"
+    #class Meta:
+        #verbose_name = "قطعه"
+        #verbose_name_plural = "قطعات"
 
-    def __str__(self):
-        return self.name
+    #def __str__(self):
+        #return self.name
 
 
 #------------------------------------------------------------------------------
-class Station(models.Model):
+#class Station(models.Model):
     #id = models.AutoField(auto_created=True, primary_key=True, verbose_name='ID')
-    name=models.CharField(max_length=400,verbose_name = "نام")
-    description=models.TextField(max_length=500,null=True, blank=True,verbose_name = "مشخصات")
+    #name=models.CharField(max_length=400,verbose_name = "نام")
+    #description=models.TextField(max_length=500,null=True, blank=True,verbose_name = "مشخصات")
     #material_input=models.ForeignKey(Material,on_delete=models.CASCADE,null=True,blank=True,verbose_name = " قطعه ورودی")
     #station_input=models.ForeignKey('Station',on_delete=models.CASCADE,null=True,blank=True,verbose_name = " ایستگاه ورودی ")
 
-    class Meta:
-        verbose_name = "ایستگاه"
-        verbose_name_plural = "ایستگاه ها"
+    #class Meta:
+        #verbose_name = "ایستگاه"
+        #verbose_name_plural = "ایستگاه ها"
 
-    def __str__(self):
-        return self.name
-
-
-#------------------------------------------------------------------------------
-class Relation(models.Model):
-    #id = models.AutoField(auto_created=True, primary_key=True, verbose_name='ID')
-    name=models.CharField(max_length=400,verbose_name = "نام")
-    description=models.TextField(max_length=500,null=True,blank=True,verbose_name = "مشخصات")
-    material_input=models.ManyToManyField(Material,verbose_name =" قطعه ورودی")
-    station_input=models.ManyToManyField(Station,verbose_name = " ایستگاه ورودی ")
-
-    class Meta:
-        verbose_name = "رابطه"
-        verbose_name_plural = "روابط"
-
-    def __str__(self):
-        return self.name
-
-
+    #def __str__(self):
+        #return self.name
 
 
 #------------------------------------------------------------------------------
-class Product(models.Model):
+#class Relation(models.Model):
     #id = models.AutoField(auto_created=True, primary_key=True, verbose_name='ID')
-    name=models.CharField(max_length=200,verbose_name = "نام")
-    description=models.TextField(max_length=800,null=True,blank=True,verbose_name = "توضیحات")
+    #name=models.CharField(max_length=400,verbose_name = "نام")
+    #description=models.TextField(max_length=500,null=True,blank=True,verbose_name = "مشخصات")
+    #material_input=models.ManyToManyField(Material,verbose_name =" قطعه ورودی")
+    #station_input=models.ManyToManyField(Station,verbose_name = " ایستگاه ورودی ")
+
+    #class Meta:
+        #verbose_name = "رابطه"
+        #verbose_name_plural = "روابط"
+
+    #def __str__(self):
+        #return self.name
+
+
+
+
+#------------------------------------------------------------------------------
+#class Product(models.Model):
+    #id = models.AutoField(auto_created=True, primary_key=True, verbose_name='ID')
+    #name=models.CharField(max_length=200,verbose_name = "نام")
+    #description=models.TextField(max_length=800,null=True,blank=True,verbose_name = "توضیحات")
     #material_input=models.ForeignKey(Material,on_delete=models.CASCADE,null=True,blank=True,verbose_name = " قطعه ورودی")
     #station_input=models.ForeignKey(Station,on_delete=models.CASCADE,null=True,blank=True,verbose_name = " ایستگاه ورودی ")
 
-    class Meta:
-        verbose_name = " محصول "
-        verbose_name_plural = " محصولات "
+    #class Meta:
+        #verbose_name = " محصول "
+        #verbose_name_plural = " محصولات "
 
-    def __str__(self):
-        return self.name
+    #def __str__(self):
+        #return self.name
