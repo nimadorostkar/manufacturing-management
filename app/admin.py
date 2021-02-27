6492f1c6402b9fd5dc2ee76970e59884b642b1d3
nimadorostkar <nimadorostkar97@gmail.com>
2021-02-27 14:48:04 +0330

    comment all admin

--- app/admin.py
+++ app/admin.py
@@ -9,17 +9,17 @@ admin.site.site_title= "Tavankar"
 
 
 
-class ProductAdmin(admin.ModelAdmin):
-	list_display = ('name','description')
-class MaterialAdmin(admin.ModelAdmin):
-	list_display = ('name','description')
-class StationAdmin(admin.ModelAdmin):
-	list_display = ('name','description')
-class RelationAdmin(admin.ModelAdmin):
-	list_display = ('name','description')
+#class ProductAdmin(admin.ModelAdmin):
+#	list_display = ('name','description')
+#class MaterialAdmin(admin.ModelAdmin):
+#	list_display = ('name','description')
+#class StationAdmin(admin.ModelAdmin):
+#	list_display = ('name','description')
+#class RelationAdmin(admin.ModelAdmin):
+#	list_display = ('name','description')
 
 
-admin.site.register(models.Product,ProductAdmin)
-admin.site.register(models.Material,MaterialAdmin)
-admin.site.register(models.Station,StationAdmin)
-admin.site.register(models.Relation,RelationAdmin)
+#admin.site.register(models.Product,ProductAdmin)
+#admin.site.register(models.Material,MaterialAdmin)
+#admin.site.register(models.Station,StationAdmin)
+#admin.site.register(models.Relation,RelationAdmin)
