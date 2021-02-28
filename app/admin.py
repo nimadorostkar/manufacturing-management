9c9ac74514ab492a5ad1065b5e65c8cdc4f5dfd6
nimadorostkar <nimadorostkar97@gmail.com>
2021-02-28 17:29:15 +0330

    ready

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from . import models
 from django.contrib.admin.models import LogEntry
-from treenode.admin import TreeNodeModelAdmin
-from treenode.forms import TreeNodeForm
-from .models import Category
+
 
 admin.site.register(LogEntry)
 admin.site.site_header= "  پنل مدیریت  "
@@ -12,46 +10,7 @@ admin.site.site_title= "Tavankar"
 
 
 
-class CategoryAdmin(TreeNodeModelAdmin):
+class PlaceAdmin(admin.ModelAdmin):
+	list_display = ('name','description')
 
-    # set the changelist display mode: 'accordion', 'breadcrumbs' or 'indentation' (default)
-    # when changelist results are filtered by a querystring,
-    # 'breadcrumbs' mode will be used (to preserve data display integrity)
-    treenode_display_mode = TreeNodeModelAdmin.TREENODE_DISPLAY_MODE_ACCORDION
-    # treenode_display_mode = TreeNodeModelAdmin.TREENODE_DISPLAY_MODE_BREADCRUMBS
-    # treenode_display_mode = TreeNodeModelAdmin.TREENODE_DISPLAY_MODE_INDENTATION
-
-    # use TreeNodeForm to automatically exclude invalid parent choices
-    form = TreeNodeForm
-
-admin.site.register(Category, CategoryAdmin)
-
-
-
-
-
-
-
-
-
-
-
-
-
-#------------------------------------------------------------------------------
-
-
-#class ProductAdmin(admin.ModelAdmin):
-#	list_display = ('name','description')
-#class MaterialAdmin(admin.ModelAdmin):
-#	list_display = ('name','description')
-#class StationAdmin(admin.ModelAdmin):
-#	list_display = ('name','description')
-#class RelationAdmin(admin.ModelAdmin):
-#	list_display = ('name','description')
-
-
-#admin.site.register(models.Product,ProductAdmin)
-#admin.site.register(models.Material,MaterialAdmin)
-#admin.site.register(models.Station,StationAdmin)
-#admin.site.register(models.Relation,RelationAdmin)
+admin.site.register(models.Place,PlaceAdmin)
--- app/models.py
+++ app/models.py
@@ -2,138 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.html import format_html
 from django.urls import reverse
-from treenode.models import TreeNodeModel
-
-
-
-
-
-
-
-
-class Category(TreeNodeModel):
-
-    # the field used to display the model instance
-    # default value 'pk'
-    treenode_display_field = 'name'
-
-    name = models.CharField(max_length=50)
-
-    class Meta(TreeNodeModel.Meta):
-        verbose_name = 'Category'
-        verbose_name_plural = 'Categories'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 
 #------------------------------------------------------------------------------
-#------------------------------------------------------------------------------
-#class Place(models.Model):
-    #CHOICES = ( ('M','Material'), ('S','Station'), ('P','Product') )
-    #position=models.CharField(max_length=1, choices=CHOICES,verbose_name = "موقعیت")
-    #name=models.CharField(max_length=400,verbose_name = "نام")
-    #description=models.TextField(max_length=500,null=True, blank=True,verbose_name = "مشخصات")
-    #location=models.CharField(max_length=400,verbose_name = "location")
-    #input=models.ManyToManyField('Place',blank=True,verbose_name = "ورودی")
+class Place(models.Model):
+    CHOICES = ( ('M','Material'), ('S','Station'), ('P','Product') )
+    position=models.CharField(max_length=1, choices=CHOICES,verbose_name = "موقعیت")
+    name=models.CharField(max_length=400,verbose_name = "نام")
+    description=models.TextField(max_length=500,null=True, blank=True,verbose_name = "مشخصات")
+    location=models.CharField(max_length=400,verbose_name = "location")
+    input=models.ManyToManyField('Place',blank=True,verbose_name = "ورودی")
 
-    #class Meta:
-        #verbose_name = "جایگاه"
-        #verbose_name_plural = " جایگاه ها"
+    class Meta:
+        verbose_name = "جایگاه"
+        verbose_name_plural = " جایگاه ها"
 
-    #def __str__(self):
-        #return self.name
-#------------------------------------------------------------------------------
-#------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-#------------------------------------------------------------------------------
-#class Material(models.Model):
-    #id = models.AutoField(auto_created=True, primary_key=True, verbose_name='ID')
-    #name=models.CharField(max_length=400,verbose_name = "نام")
-    #description=models.TextField(max_length=500,null=True, blank=True,verbose_name = "مشخصات")
-    #material_input=models.ForeignKey('Material',on_delete=models.CASCADE,null=True,blank=True,verbose_name = " قطعه ورودی")
-    #station_input=models.ForeignKey('Station',on_delete=models.CASCADE,null=True,blank=True,verbose_name = " ایستگاه ورودی ")
-
-    #class Meta:
-        #verbose_name = "قطعه"
-        #verbose_name_plural = "قطعات"
-
-    #def __str__(self):
-        #return self.name
-
-
-#------------------------------------------------------------------------------
-#class Station(models.Model):
-    #id = models.AutoField(auto_created=True, primary_key=True, verbose_name='ID')
-    #name=models.CharField(max_length=400,verbose_name = "نام")
-    #description=models.TextField(max_length=500,null=True, blank=True,verbose_name = "مشخصات")
-    #material_input=models.ForeignKey(Material,on_delete=models.CASCADE,null=True,blank=True,verbose_name = " قطعه ورودی")
-    #station_input=models.ForeignKey('Station',on_delete=models.CASCADE,null=True,blank=True,verbose_name = " ایستگاه ورودی ")
-
-    #class Meta:
-        #verbose_name = "ایستگاه"
-        #verbose_name_plural = "ایستگاه ها"
-
-    #def __str__(self):
-        #return self.name
-
-
-#------------------------------------------------------------------------------
-#class Relation(models.Model):
-    #id = models.AutoField(auto_created=True, primary_key=True, verbose_name='ID')
-    #name=models.CharField(max_length=400,verbose_name = "نام")
-    #description=models.TextField(max_length=500,null=True,blank=True,verbose_name = "مشخصات")
-    #material_input=models.ManyToManyField(Material,verbose_name =" قطعه ورودی")
-    #station_input=models.ManyToManyField(Station,verbose_name = " ایستگاه ورودی ")
-
-    #class Meta:
-        #verbose_name = "رابطه"
-        #verbose_name_plural = "روابط"
-
-    #def __str__(self):
-        #return self.name
-
-
-
-
-#------------------------------------------------------------------------------
-#class Product(models.Model):
-    #id = models.AutoField(auto_created=True, primary_key=True, verbose_name='ID')
-    #name=models.CharField(max_length=200,verbose_name = "نام")
-    #description=models.TextField(max_length=800,null=True,blank=True,verbose_name = "توضیحات")
-    #material_input=models.ForeignKey(Material,on_delete=models.CASCADE,null=True,blank=True,verbose_name = " قطعه ورودی")
-    #station_input=models.ForeignKey(Station,on_delete=models.CASCADE,null=True,blank=True,verbose_name = " ایستگاه ورودی ")
-
-    #class Meta:
-        #verbose_name = " محصول "
-        #verbose_name_plural = " محصولات "
-
-    #def __str__(self):
-        #return self.name
+    def __str__(self):
+        return self.name
