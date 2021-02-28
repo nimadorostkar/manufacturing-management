d609276d90fffef47aaeded3b3aaa36d5940456f
nimadorostkar <nimadorostkar97@gmail.com>
2021-02-28 14:35:33 +0330

    show

--- app/views.py
+++ app/views.py
@@ -47,5 +47,5 @@ def pages(request):
 
 @login_required(login_url="/login/")
 def show(request):
-    places=nimaaaa
+    cat=models.Category.objects.get_ancestors()
     return render(request, 'show.html', {'places': places,'child': child})
