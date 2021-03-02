0561351cbf6326ef1192456bb04d71c578621c7e
nima <54316106+nimadorostkar@users.noreply.github.com>
2021-03-02 18:14:46 +0330

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, re_path
 from app import views
 
 
+
 urlpatterns = [
     # Matches any html file
     re_path(r'^.*\.html', views.pages, name='pages'),
-
     # The home page
     path('', views.index, name='home'),
     path('show', views.show, name='show'),
 ]
+
