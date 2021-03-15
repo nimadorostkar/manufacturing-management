f75ee1a255675ddc11b4cf4e32c6ab339d87b8df
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-15 08:49:04 +0330

    sss

--- app/models.py
+++ app/models.py
@@ -9,11 +9,11 @@ from treebeard.mp_tree import MP_Node
 #------------------------------------------------------------------------------
 class Station(models.Model):
     CHOICES = ( ('M','Material'), ('S','Station'), ('P','Product') )
-    position=models.CharField(max_length=1, choices=CHOICES,verbose_name = "موقعیت")
+    position=models.CharField(max_length=1,choices=CHOICES,verbose_name = "موقعیت")
     name=models.CharField(max_length=400,verbose_name = "نام")
     description=models.TextField(max_length=500,null=True, blank=True,verbose_name = "مشخصات")
     location=models.CharField(max_length=400,verbose_name = "location")
-    input=models.ForeignKey('self',on_delete=models.CASCADE,blank=True,verbose_name = "ورودی")
+    input=models.ForeignKey('Station',on_delete=models.CASCADE,blank=True,verbose_name = "ورودی")
 
     class Meta:
         verbose_name = "ایستگاه"
