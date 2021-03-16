c829e4cca97e4c243a87f75533ac2a64c7f4a01b
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-16 12:38:29 +0330

    edit and create beter and clean

--- app/admin.py
+++ app/admin.py
@@ -2,13 +2,9 @@ from django.contrib import admin
 from . import models
 from django.contrib.admin.models import LogEntry
 
-from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
+from mptt.admin import DraggableMPTTAdmin
 from app.models import Node
-
-
-
-
 
 
 
@@ -20,16 +16,22 @@ admin.site.site_title= "Tavankar"
 
 
 
+#------------------------------------------------------------------------------
 class StationAdmin(admin.ModelAdmin):
 	list_display = ('name','description')
-
-class CustomMPTTModelAdmin(MPTTModelAdmin):
-    # specify pixel amount for this ModelAdmin only:
-    mptt_level_indent = 20
-
-
 
 admin.site.register(models.Station,StationAdmin)
 
 
-admin.site.register(Node, CustomMPTTModelAdmin)
+
+
+#------------------------------------------------------------------------------
+class CustomMPTTModelAdmin(MPTTModelAdmin):
+    mptt_level_indent = 20   # specify pixel amount for this ModelAdmin only
+    #mptt_indent_field = "some_node_field"
+
+
+admin.site.register(Node,DraggableMPTTAdmin,
+    list_display=('tree_actions','indented_title',),
+    list_display_links=('indented_title',),
+	)
--- app/models.py
+++ app/models.py
@@ -2,10 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.html import format_html
 from django.urls import reverse
-
-from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
-
 
 
 
