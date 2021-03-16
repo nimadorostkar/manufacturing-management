1e7a6c772f8f16f044b8b4ea29e92e346055fcaa
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-16 12:25:47 +0330

    create admin for mptt

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from . import models
 from django.contrib.admin.models import LogEntry
 
+from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from myproject.myapp.models import Node
+
+
 
 
 
@@ -18,6 +23,13 @@ admin.site.site_title= "Tavankar"
 class StationAdmin(admin.ModelAdmin):
 	list_display = ('name','description')
 
+class CustomMPTTModelAdmin(MPTTModelAdmin):
+    # specify pixel amount for this ModelAdmin only:
+    mptt_level_indent = 20
+
 
 
 admin.site.register(models.Station,StationAdmin)
+
+
+admin.site.register(Node, CustomMPTTModelAdmin)
