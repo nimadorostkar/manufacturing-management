97718a411dea4c8fdd89163187d620b19b6263a0
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-18 10:52:11 +0330

    add loc to settings

--- core/settings.py
+++ core/settings.py
@@ -98,6 +98,44 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+############################################################
+# location :
+
+LOCATION_FIELD_PATH = settings.STATIC_URL + 'location_field'
+
+LOCATION_FIELD = {
+    'map.provider': 'google',
+    'map.zoom': 13,
+
+    'search.provider': 'google',
+    'search.suffix': '',
+
+    # Google
+    'provider.google.api': '//maps.google.com/maps/api/js?sensor=false',
+    'provider.google.api_key': '',
+    'provider.google.api_libraries': '',
+    'provider.google.map.type': 'ROADMAP',
+
+    # Mapbox
+    'provider.mapbox.access_token': '',
+    'provider.mapbox.max_zoom': 18,
+    'provider.mapbox.id': 'mapbox.streets',
+
+    # OpenStreetMap
+    'provider.openstreetmap.max_zoom': 18,
+
+    # misc
+    'resources.root_path': LOCATION_FIELD_PATH,
+    'resources.media': {
+        'js': (
+            LOCATION_FIELD_PATH + '/js/form.js',
+        ),
+    },
+}
+
+############################################################
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
