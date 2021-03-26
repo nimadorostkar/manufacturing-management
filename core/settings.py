9a412ce26c4a319e47a346cdb150089f352f11c3
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-26 22:33:33 +0430

    add grappelli to urls

--- core/settings.py
+++ core/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
