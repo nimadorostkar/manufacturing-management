2b1ef749fe8160fc9867aae06cf52757de00d5f3
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-29 14:10:15 +0430

    add account template

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'app',     # Enable the inner app
     'mptt',    # https://django-mptt.readthedocs.io/en/latest/index.html
     'mapbox_location_field',  # https://github.com/simon-the-shark/django-mapbox-location-field
