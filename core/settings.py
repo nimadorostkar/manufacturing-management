cc62ecc36cd9f8b97ca8da498aa03eb004896f1e
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-13 22:06:41 +0430

    MEDIA_ROOT

--- core/settings.py
+++ core/settings.py
@@ -215,5 +215,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'core/static'),
 )
+
+
+MEDIA_ROOT= os.path.join(BASE_DIR, 'static')
+MEDIA_URL='/media/'
+
 #############################################################
 #############################################################
