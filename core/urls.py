6bc9dcc8f6375b76b721f9c99432b7dc542293ce
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-13 22:44:02 +0430

    r

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include  # add this
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     #path('grappelli/', include('grappelli.urls')), # grappelli URLS
@@ -7,3 +11,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path("", include("app.urls"))  # add this
 ]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
