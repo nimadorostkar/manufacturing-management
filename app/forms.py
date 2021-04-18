1590b3a142d9c87b52020cfcf6575d9781882f92
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-18 18:51:26 +0430

    Ticket to forms

--- app/forms.py
+++ app/forms.py
@@ -22,3 +22,10 @@ class UserForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username','first_name','last_name','email','password1','password2']
+
+
+#------------------------------------------------------------------------------
+class TicketForm(forms.ModelForm):
+	class Meta:
+		model = Ticket
+		fields = ['to','title','descriptions']
