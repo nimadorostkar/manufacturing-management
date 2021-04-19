ebe27d0d5e26ec11bcd6dfe5c279ac44cee3aba7
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-20 02:34:33 +0430

    reset data base

--- app/models.py
+++ app/models.py
@@ -106,8 +106,8 @@ class Product(models.Model):
 #------------------------------------------------------------------------------
 class Manufacture(models.Model):
     product = models.OneToOneField(Product, on_delete=models.CASCADE,verbose_name = " محصول ")
-    code = models.AutoField(primary_key=True, verbose_name = "کد ")
-    #code=models.CharField(max_length=50,null=True, blank=True,verbose_name = "کد ")
+    #code = models.AutoField(primary_key=True, verbose_name = "کد ")
+    code=models.CharField(max_length=50,null=True, blank=True,verbose_name = "کد ")
     description = models.TextField(max_length=900,null=True, blank=True,verbose_name = "توضیحات")
     circulation = models.IntegerField(default='1',verbose_name = " تیراژ ")
 
