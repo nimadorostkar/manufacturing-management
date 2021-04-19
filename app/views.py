db50ae39ccf2abca03b06f3b013838b8a62aee68
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-19 15:56:51 +0430

    g

--- app/views.py
+++ app/views.py
@@ -186,6 +186,8 @@ def ticket(request):
             obj.title = ticket_form.cleaned_data['title']
             obj.descriptions = ticket_form.cleaned_data['descriptions']
             obj.to = ticket_form.cleaned_data['to']
+            obj.ticket_number = ticket_form.cleaned_data['ticket_number']
+            obj.created_on = ticket_form.cleaned_data['created_on']
             obj.user = ticket_form.created_by=request.user
             obj.save()
             #messages.success(request, _('done successfully !'))
