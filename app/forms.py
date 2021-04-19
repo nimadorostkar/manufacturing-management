83f3500ce451e81b55c6684b4ad131bfa3904e7c
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-19 17:45:27 +0430

    push

--- app/forms.py
+++ app/forms.py
@@ -27,7 +27,7 @@ class UserForm(UserCreationForm):
 
 #------------------------------------------------------------------------------
 class TicketForm(forms.ModelForm):
-	to = forms.ModelChoiceField(queryset=User.objects.all(), widget=forms.Select(), error_messages={'required': 'این فیلد ضروری است'})
+	to = forms.ModelChoiceField(queryset=User.objects.all(), widget=forms.Select(), error_messages={'required': 'این فیلد ضروری است'} )
 
 	class Meta:
 		model = Ticket
