a860c435fcd77539ccdc8a2b0d61b1f7a374244b
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-20 19:53:48 +0430

    pip install django-admin-shortcuts

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,6 @@ AUTHENTICATION_BACKENDS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- static/admin/js/admin/RelatedObjectLookups.js
+++ static/admin/js/admin/RelatedObjectLookups.js
@@ -32,7 +32,8 @@
                 href += '&_popup=1';
             }
         }
-        var win = window.open(href, name, 'height=500,width=800,resizable=yes,scrollbars=yes');
+        // GRAPPELLI CUSTOM: changed width
+        var win = window.open(href, name, 'height=500,width=1000,resizable=yes,scrollbars=yes');
         win.focus();
         return false;
     }
@@ -49,6 +50,8 @@
         } else {
             document.getElementById(name).value = chosenId;
         }
+        // GRAPPELLI CUSTOM: element focus
+        elem.focus();
         win.close();
     }
 
@@ -58,7 +61,8 @@
 
     function updateRelatedObjectLinks(triggeringLink) {
         var $this = $(triggeringLink);
-        var siblings = $this.nextAll('.view-related, .change-related, .delete-related');
+        // GRAPPELLI CUSTOM: use parent before nextAll
+        var siblings = $this.parent().nextAll().find('.view-related, .change-related, .delete-related');
         if (!siblings.length) {
             return;
         }
@@ -86,6 +90,8 @@
                 } else {
                     elem.value = newId;
                 }
+                // GRAPPELLI CUSTOM: element focus
+                elem.focus();
             }
             // Trigger a change event to update related links if required.
             $(elem).trigger('change');
@@ -100,6 +106,7 @@
 
     function dismissChangeRelatedObjectPopup(win, objId, newRepr, newId) {
         var id = windowname_to_id(win.name).replace(/^edit_/, '');
+        var elem = document.getElementById(id);
         var selectsSelector = interpolate('#%s, #%s_from, #%s_to', [id, id, id]);
         var selects = $(selectsSelector);
         selects.find('option').each(function() {
@@ -108,6 +115,8 @@
                 this.value = newId;
             }
         });
+        // GRAPPELLI CUSTOM: element focus
+        elem.focus();
         selects.next().find('.select2-selection__rendered').each(function() {
             // The element can have a clear button as a child.
             // Use the lastChild to modify only the displayed value.
@@ -119,6 +128,7 @@
 
     function dismissDeleteRelatedObjectPopup(win, objId) {
         var id = windowname_to_id(win.name).replace(/^delete_/, '');
+        var elem = document.getElementById(id);
         var selectsSelector = interpolate('#%s, #%s_from, #%s_to', [id, id, id]);
         var selects = $(selectsSelector);
         selects.find('option').each(function() {
@@ -126,8 +136,19 @@
                 $(this).remove();
             }
         }).trigger('change');
+        // GRAPPELLI CUSTOM: element focus
+        elem.focus();
         win.close();
     }
+
+    // GRAPPELLI CUSTOM
+    function removeRelatedObject(triggeringLink) {
+        var id = triggeringLink.id.replace(/^remove_/, '');
+        var elem = document.getElementById(id);
+        elem.value = "";
+        elem.focus();
+    }
+    window.removeRelatedObject = removeRelatedObject;
 
     // Global for testing purposes
     window.id_to_windowname = id_to_windowname;
@@ -167,7 +188,10 @@
                 updateRelatedObjectLinks(this);
             }
         });
-        $('.related-widget-wrapper select').trigger('change');
+        // GRAPPELLI CUSTOM
+        /* triggering select means that update_lookup is triggered with
+        generic autocompleted (which would empty the field) */
+        $('.grp-related-widget-tools').parent().children('.grp-related-widget').children('select:first-child').trigger('change');
         $('body').on('click', '.related-lookup', function(e) {
             e.preventDefault();
             var event = $.Event('django:lookup-related');
@@ -178,4 +202,4 @@
         });
     });
 
-})(django.jQuery);
+})(grp.jQuery);
