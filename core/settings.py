99fc255a0b394ebb29dfa94e3a0ee35788cf71f0
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-24 12:04:07 +0430

    push

--- core/settings.py
+++ core/settings.py
@@ -4,7 +4,6 @@ from unipath import Path
 import dj_database_url
 
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR    = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = Path(__file__).parent
