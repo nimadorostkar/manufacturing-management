c35a1521f564c9aca9af6ffd3cbc8bc40675675b
nimadorostkar <nimadorostkar97@gmail.com>
2021-04-27 18:07:16 +0430

    edit urls

--- app/urls.py
+++ app/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('stations', views.stations, name='stations'),
     path('stations_detail/<int:id>/',views.stations_detail,name='stations_detail'),
     # manufactures
-    path('order', views.orders, name='order'),
+    path('order', views.order, name='order'),
     path('orders_detail/<int:id>/',views.orders_detail,name='orders_detail'),
 ]
