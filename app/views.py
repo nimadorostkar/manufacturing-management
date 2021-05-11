8d1c55d23d613978f6c7e3663019d665f883b75f
nimadorostkar <nimadorostkar97@gmail.com>
2021-05-11 17:33:54 +0430

    {{ Order.product }}

--- app/views.py
+++ app/views.py
@@ -150,7 +150,7 @@ def processes_detail(request, id):
     process = get_object_or_404(models.Process, id=id)
     processes= models.Process.objects.all()
     input = models.Tree.objects.filter(name=process)
-    order = models.Order.objects.filter(product_name=input)
+    order = models.Order.objects.all()
 
     return render(request, 'processes_detail.html', {
     'process': process,
