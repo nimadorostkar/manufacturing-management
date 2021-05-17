a54d3b9be6a8d345970fdaedcdefd85d73739bfb
nimadorostkar <nimadorostkar97@gmail.com>
2021-05-17 13:26:49 +0430

    eee

--- app/views.py
+++ app/views.py
@@ -132,16 +132,14 @@ def processes(request):
 @login_required()
 def processes_detail(request, id):
     process = get_object_or_404(models.Process, id=id)
+    processes= models.Process.objects.all()
     input = models.Tree.objects.filter(name=process)
-    order = models.Order.objects.all()
-    orders = models.Order.objects.filter(product__name=input)
-
     return render(request, 'processes_detail.html', {
     'process': process,
-    'input': input,
-    'order':order,
-    'orders':orders
+    'processes': processes,
+    'input': input
     })
+
 
 
 ############################# profile ########################################
