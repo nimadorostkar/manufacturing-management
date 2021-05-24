e9d913be3088631eac9564ac87b7095ec35536d2
nimadorostkar <nimadorostkar97@gmail.com>
2021-05-24 16:26:57 +0430

    e

--- app/views.py
+++ app/views.py
@@ -135,9 +135,16 @@ def processes_detail(request, id):
     process = get_object_or_404(models.Process, id=id)
     processes= models.Process.objects.all()
     input = models.Tree.objects.filter(name=process)
+
+    orders = models.Order.objects.filter(product = input.relatedProduct )
+
+    nodes = models.Tree.objects.all()
+
     return render(request, 'processes_detail.html', {
     'process': process,
     'processes': processes,
+    'orders': orders,
+    'nodes': nodes,
     'input': input
     })
 
