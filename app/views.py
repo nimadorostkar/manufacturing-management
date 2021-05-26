57ecea547310a180e03e46e76955f5e1ff71d5c7
nimadorostkar <nimadorostkar97@gmail.com>
2021-05-26 13:23:28 +0430

    use .values to filter just relatedProduct__name

--- app/views.py
+++ app/views.py
@@ -136,6 +136,8 @@ def processes_detail(request, id):
     process = get_object_or_404(models.Process, id=id)
     processes= models.Process.objects.all()
     input = models.Tree.objects.filter(name=process)
+    a = models.Tree.objects.filter(name=process).values('relatedProduct__name')
+    print(a)
     orders = models.Order.objects.all()
     return render(request, 'processes_detail.html', {
     'process': process,
