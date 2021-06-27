462a74fe01e9593f67e477bee7d623dd9b158473
nimadorostkar <nimadorostkar97@gmail.com>
2021-06-27 16:44:10 +0430

    import Confirmation in forms

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from allauth.account.forms import SignupForm
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
-from .models import Profile, Ticket, Process
+from .models import Profile, Ticket, Process, Confirmation
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 
